d5b1dbf885ccb4c66d389e7a82ad30f20e3fe2e8
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-11-23 14:48:18 +0100

    Update vmapper.sh

--- scrips/vmapper.sh
+++ scrips/vmapper.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
-# version 14.7.9
+# version 14.8.0
 
 #Version checks
 Ver49vmapper="1.6.0"
 Ver56vmwatchdog="1.3.9"
-VerATVwebhook="2.1.1"
+VerATVwebhook="2.1.2"
 
 #Create logfile
 if [ ! -e /sdcard/vm.log ] ;then
